8dee03f092b5c56cbe2b243c639c4093eaa00d43
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-30 21:41:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+class Application 
+  
+  @@items = ["Bananas", "Carrots", "Apples", "Kiwis"]
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+end 
